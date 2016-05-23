c273f0f0713cd11fc5412a023f7303b3686baaab
Mattias Festin <mattias@mirror.se>
2016-05-23 10:47:54 +0200

    documentation generator

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,23 @@
 /// <reference path="../typings/tsd.d.ts" />
+/**
+ * This is a doc comment for a dynamic module.
+ */
 
+/**
+ * Example comment:
+ * My foo should be 123
+ */
 export const foo = 123;
+
+/**
+ * Example comment:
+ * By bar should be 456
+ */
 export const bar = 123;
 
 console.log(foo);
+
+/**
+ * This is a doc comment for "someVar".
+ */
+var someVar:string = "value";
